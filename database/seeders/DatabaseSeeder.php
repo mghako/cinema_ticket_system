96aaa722507afc839662d1b658542996c8749257
mghako <htetaungko@yahoo.com>
2021-10-23 12:49:17 +0630

    added movie data with factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        \App\Models\City::factory(2)->create();
+        // \App\Models\City::factory(2)->create();
         // \App\Models\Cinema::factory(10)->create();
         // \App\Models\CinemaHall::factory(10)->create([
         //     'cinema_id' => 1
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\CinemaSeat::factory(8)->create([
         //     'cinema_hall_id' => 1
         // ]);
-        // \App\Models\Movie::factory(10)->create();
+        \App\Models\Movie::factory(10)->create();
         // \App\Models\Show::factory(4)->create([
         //     'cinema_hall_id' => 1,
         //     'movie_id' => 1
